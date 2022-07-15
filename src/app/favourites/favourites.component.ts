8e3c5d447ad85c0bca3bdae83a1f985cbdae799c
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-07-15 09:47:31 +0100

    home button css updated

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,25 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FavouritesService } from '../favourites.service';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
-
+import { Component, OnInit } from '@angular/core'
+import { FavouritesService } from '../favourites.service'
+import { faHouse, faBone } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
-  styleUrls: ['./favourites.component.scss']
+  styleUrls: ['./favourites.component.scss'],
 })
-
 export class FavouritesComponent implements OnInit {
-  faHouse = faHouse;
-  fasHeart = fasHeart;
-  favouriteImages = this.favouritesService.getFavouriteImages();
+  faHouse = faHouse
+  fasHeart = fasHeart
+  faBone = faBone
+  favouriteImages = this.favouritesService.getFavouriteImages()
 
-  constructor(
-    private favouritesService: FavouritesService
-  ) { }
+  constructor(private favouritesService: FavouritesService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
